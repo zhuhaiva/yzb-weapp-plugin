0e42b1b1f7163558f65cb415442e96e376a8983c
zhuhai <501611944@qq.com>
2025-08-29 14:36:26 +0800

    添加小程序基本结构和页面，配置插件

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,39 @@
+var plugin = requirePlugin("yzb-plugin");
+
+Page({
+  data: {
+    items: [],
+    currentItem: 0
+  },
+  onLoad(options) {
+    const {
+      miniProgram
+    } = wx.getAccountInfoSync()
+    plugin.config({
+      ...options,
+      appId: miniProgram.appId,
+      type: 1000,
+      wxscene: getApp().scene,
+      mtoken: "204b2101-e7a0-4b08-9c54-7aa1ebd181a1",
+      userInfo:{
+        nickName:"develop"
+      }
+    }).then(({
+      url
+    }) => {
+      wx.redirectTo({
+        url,
+        fail(res) {
+          console.log(res)
+        }
+      })
+    })
+  },
+  addItem() {
+    this.data.items.push(this.data.currentItem++)
+    this.setData({
+      items: this.data.items,
+      currentItem: this.data.currentItem
+    })
+  }
+})
